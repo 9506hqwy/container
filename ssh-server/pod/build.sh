6fcfd7a8ec87dfcf3b0d0eef8f0a7090b1180ee5
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2023-10-10 00:55:19 +0900

    ssh サーバのコンテナを追加した。

---
+++ ssh-server/pod/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -eux
+
+export SSH_USER_NAME=$(podman image inspect ssh | grep '"SSH_USER_NAME=' | cut -d '=' -f 2 | tr -d '"')
+
+export HOST_PRIVATE_KEY=$(cat ssh_host_ed25519_key | base64 -w 0)
+export HOST_PUBLIC_KEY=$(cat ssh_host_ed25519_key.pub | base64 -w 0)
+
+export USER_PRIVATE_KEY=$(cat ${SSH_USER_NAME}_ed25519_key.pub | base64 -w 0)
+
+envsubst < server.yml | podman play kube - $@
