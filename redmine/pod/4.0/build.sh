f562b5cfa74a31c55126805a6cc03a30ec6de03f
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2023-10-18 01:18:26 +0900

    Redmine 4.0 のコンテナを追加した。

---
+++ redmine/pod/4.0/build.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+set -eux
+
+case "$1" in
+    mysql)
+        if [[ -z ${MYSQL_USER} ]] ; then
+            echo 'Specify DB username to $MYSQL_USER'
+        fi
+
+        if [[ -z ${MYSQL_PASSWORD} ]] ; then
+            echo 'Specify DB password to $MYSQL_PASSWORD'
+        fi
+
+        export DB_USER=$(echo -n $MYSQL_USER | base64 -w 0)
+        export DB_PASSWORD=$(echo -n $MYSQL_PASSWORD | base64 -w 0)
+        ;;
+
+    postgresql)
+        if [[ -z ${POSTGRES_USER} ]] ; then
+            echo 'Specify DB username to $POSTGRES_USER'
+        fi
+
+        if [[ -z ${POSTGRES_PASSWORD} ]] ; then
+            echo 'Specify DB password to $POSTGRES_PASSWORD'
+        fi
+
+        export DB_USER=$(echo -n $POSTGRES_USER | base64 -w 0)
+        export DB_PASSWORD=$(echo -n $POSTGRES_PASSWORD | base64 -w 0)
+        ;;
+
+    sqlite)
+        ;;
+
+    *)
+        echo 'Specify database [postgres/mysql/sqlite]'
+        exit 1
+        ;;
+esac
+
+DATABASE=$1
+shift
+
+if [[ ! -d ${HOST_PATH} ]] ; then
+    echo 'Specify directory to $HOST_PATH'
+    exit 1
+fi
+
+if [[ ! -d ${HOST_PATH}/db ]] ; then
+    echo "Create ${HOST_PATH}/db"
+    exit 1
+fi
+
+if [[ ! -d ${HOST_PATH}/files ]] ; then
+    echo "Create ${HOST_PATH}/files"
+    exit 1
+fi
+
+if [[ ! -d ${HOST_PATH}/log ]] ; then
+    echo "Create ${HOST_PATH}/log"
+    exit 1
+fi
+
+if [[ ! -d ${HOST_PATH}/plugins ]] ; then
+    echo "Create ${HOST_PATH}/plugins"
+    exit 1
+fi
+
+export DB_CIPHER_KEY=$(cat /dev/random | fold -w 12 | base64 | head -n 1)
+
+envsubst < ${DATABASE}.yml | podman play kube - $@
