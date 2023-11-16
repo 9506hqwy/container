7dee05ab26c5a1a9c238d17843c847407f61e9be
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2023-11-17 00:37:26 +0900

    GROWI に draw-image-export2 を追加した。

--- drawio/image/setup_postconfig.sh
+++ drawio/image/setup_postconfig.sh
@@ -4,6 +4,10 @@ set -eux
 
 CONFIG="$CATALINA_HOME/webapps/draw/js/PostConfig.js"
 
+if [ -n "${EXPORT_URL}" ] ; then
+    echo "window.EXPORT_URL = '${EXPORT_URL}';" >> "${CONFIG}"
+fi
+
 if [ -n "${PLANT_URL}" ] ; then
     echo "window.PLANT_URL = '${PLANT_URL}';" >> "${CONFIG}"
     echo 'EditorUi.enablePlantUml = true;' >> "${CONFIG}"
