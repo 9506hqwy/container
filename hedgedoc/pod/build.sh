1bce977c32104cb900da2a40b8ee4ab992bbae05
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2025-02-03 20:36:54 +0900

    codimd を hedgedoc に変更した。

--- hedgedoc/pod/build.sh
+++ hedgedoc/pod/build.sh
@@ -29,6 +29,6 @@ fi
 
 export DB_USER=$(echo -n $POSTGRES_USER | base64 -w 0)
 export DB_PASSWORD=$(echo -n $POSTGRES_PASSWORD | base64 -w 0)
-export CMD_DB_URL=$(echo -n "postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@127.0.0.1/codimd" | base64 -w 0)
+export CMD_DB_URL=$(echo -n "postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@127.0.0.1/hedgedoc" | base64 -w 0)
 
 envsubst < server.yml '$HOST_PATH $DB_USER $DB_PASSWORD $CMD_DB_URL' | podman play kube - $@
