0991a4a3b0f720a12bbe3e1b8ef00873c8616776
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2026-02-02 22:26:51 +0900

    grafana loki のコンテナを追加した。

---
+++ grafana-loki/pod/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eux
+
+if [[ ! -d ${HOST_PATH} ]] ; then
+    echo 'Specify directory to $HOST_PATH'
+    exit 1
+fi
+
+if [[ ! -d ${HOST_PATH}/grafana ]] ; then
+    echo "Create ${HOST_PATH}/grafana"
+    exit 1
+fi
+
+envsubst < server.yml | podman play kube - $@
